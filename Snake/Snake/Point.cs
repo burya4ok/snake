038b320019d9d840af4944d8cbeb4bd980b2c5e3
burya4ok <seregatu20111@gmail.com>
2016-01-16 18:01:00 +0200

    add constructor

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -12,6 +12,14 @@ namespace Snake
         public int y;
         public char sym;
 
+        public Point(int x, int y, char sym)
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+            Console.WriteLine("Создана новая точка!");
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);

dc28caec424d864758c5301f3944011b48b4ade1
burya4ok <seregatu20111@gmail.com>
2016-01-17 21:51:29 +0200

    Now snake can move

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -51,5 +51,11 @@ namespace Snake
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
         }
+
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
+        }
     }
 }
--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -8,9 +8,11 @@ namespace Snake
 {
     class Snake : Figure
     {
-        public Snake(Point tail, int lenght, Direction direction)
+        Direction direction;
+        public Snake(Point tail, int lenght, Direction _direction)
         {
             pList = new List<Point>();
+            direction = _direction;
 
             for (int i = 0; i < lenght; i++)
             {
@@ -18,6 +20,25 @@ namespace Snake
                 p.Move(i, direction);
                 pList.Add(p);
             }
+        }
+
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = getNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+
+        }
+        public Point getNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
         }
     }
 }
